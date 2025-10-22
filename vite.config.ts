f8b098d1263a6cc46cb21a47b022b36200254371
Talha <talha2akkaya@gmail.com>
2025-10-22 14:02:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/rflos/',
   plugins: [react()],
   server: {
     host: true,
